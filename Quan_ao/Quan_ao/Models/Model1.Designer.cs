9ae8e3e48a6b87c1fbbbfe4cc108440200ae208c
HIEU NGUYEN THANH <thanhhieu961906@gmail.com>
2023-09-19 17:58:55 +0700

    19/9

--- Quan_ao/Quan_ao/Models/Model1.Designer.cs
+++ Quan_ao/Quan_ao/Models/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\_Shop_quan_ao\Quan_ao\Quan_ao\Models\Model1.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\Back_end_ASP\Quan_ao\Quan_ao\Models\Model1.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
